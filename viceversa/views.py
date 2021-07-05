5e45c3c1769c8af60b20e34f359c722efbb25157
Illia <khirilov.il@gmail.com>
2021-07-05 19:31:00 +0300

    Features with reversed text and counting words

---
+++ viceversa/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    words_number = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text,
+                                            'wordsnumber': words_number})
